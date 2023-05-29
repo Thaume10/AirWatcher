2deea2d75af9b1a45219ea0822fc8046f432c84e
totoletoto69 <thomas.boyer69100@gmail.com>
2023-05-29 21:32:02 +0200

    plus d'erreurs

--- src/Data.cpp
+++ src/Data.cpp
@@ -29,63 +29,63 @@ vector<User>& Data::getUsers(){
   return users;
 }
 
- bool comparerParDouble(const std::pair<Sensor, double>& paire1, const std::pair<Sensor, double>& paire2) {
-    return paire1.second > paire2.second;
-}
-
-vector<pair<Sensor, double>> get_five_nearest_sensors(GPS coord){
-    vector<pair<Sensor, double>> top5;
-    vector<Sensor>::iterator itDebut = sensors.begin();
-    vector<Sensor>::iterator itFin = sensors.end();
-    for (auto it = itDebut; it != itFin; ++it) {
-        if(top5.size()<5){
-            if(it->get_is_malfunctionning()==false){
-                 top5.push_back(*it);
-            }
-        }else{
-            sort(top5.begin(), top5.end(), comparerParDouble);
-            if(it->get_is_malfunctionning()==false && top5[0].second > calculerDistance(coord,it->get_coord)){
-                top5.push_back(*it);
-            }
-        }
-        
-    }
-    return top5;
-}
-
- double calculerDistance(GPS coord1, GPS coord2) {
-    double deltaX = coord2.get_latitude() - coord1.get_latitude();
-    double deltaY = coord2.get_longitude() - coord1.get_longitude();
-    double distance = std::sqrt(deltaX * deltaX + deltaY * deltaY);
-    return distance;
-}
-
-
-vector<Measurement> get_measures_of_sensor(string sensorId,  Date start, Date end){
-    vector<Measurement> result;
-    Sensor sensor = getSensorById(sensorId);
-    vector<Measurement>::iterator itDebut = sensor.get_measurements().begin();
-    vector<Measurement>::iterator itFin = sensor.get_measurements().end();
-    for (auto it = itDebut; it != itFin; ++it) {
-        if(it->get_timestamp < end && it->get_timestamp>start){
-            result.push_back(*it);
-        }
-    }
-    return result;
-}
-
-Sensor getSensorById(string sensorId){
-    vector<Sensor>::iterator itDebut = sensors.begin();
-    vector<Sensor>::iterator itFin = sensors.end();
-    bool fin =false;
-    while (itDebut != itFin && !fin) {
-        if (itDebut->get_id() == sensorId) {
-            return *itDebut;
-        }
-        ++itDebut;
-    }
-    return Sensor();
-}
+//  bool comparerParDouble(const std::pair<Sensor, double>& paire1, const std::pair<Sensor, double>& paire2) {
+//     return paire1.second > paire2.second;
+// }
+//
+// vector<pair<Sensor, double>> get_five_nearest_sensors(GPS coord){
+//     vector<pair<Sensor, double>> top5;
+//     vector<Sensor>::iterator itDebut = sensors.begin();
+//     vector<Sensor>::iterator itFin = sensors.end();
+//     for (auto it = itDebut; it != itFin; ++it) {
+//         if(top5.size()<5){
+//             if(it->get_is_malfunctionning()==false){
+//                  top5.push_back(*it);
+//             }
+//         }else{
+//             sort(top5.begin(), top5.end(), comparerParDouble);
+//             if(it->get_is_malfunctionning()==false && top5[0].second > calculerDistance(coord,it->get_coord)){
+//                 top5.push_back(*it);
+//             }
+//         }
+//         
+//     }
+//     return top5;
+// }
+//
+//  double calculerDistance(GPS coord1, GPS coord2) {
+//     double deltaX = coord2.get_latitude() - coord1.get_latitude();
+//     double deltaY = coord2.get_longitude() - coord1.get_longitude();
+//     double distance = std::sqrt(deltaX * deltaX + deltaY * deltaY);
+//     return distance;
+// }
+//
+//
+// vector<Measurement> get_measures_of_sensor(string sensorId,  Date start, Date end){
+//     vector<Measurement> result;
+//     Sensor sensor = getSensorById(sensorId);
+//     vector<Measurement>::iterator itDebut = sensor.get_measurements().begin();
+//     vector<Measurement>::iterator itFin = sensor.get_measurements().end();
+//     for (auto it = itDebut; it != itFin; ++it) {
+//         if(it->get_timestamp < end && it->get_timestamp>start){
+//             result.push_back(*it);
+//         }
+//     }
+//     return result;
+// }
+//
+// Sensor getSensorById(string sensorId){
+//     vector<Sensor>::iterator itDebut = sensors.begin();
+//     vector<Sensor>::iterator itFin = sensors.end();
+//     bool fin =false;
+//     while (itDebut != itFin && !fin) {
+//         if (itDebut->get_id() == sensorId) {
+//             return *itDebut;
+//         }
+//         ++itDebut;
+//     }
+//     return Sensor();
+// }
 
 
 
