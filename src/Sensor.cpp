7e6d0a0ce6dc98acc2561010bb4cfabdc4f852dc
Léo Aimonetto <leoaimonetto@gmail.com>
2023-05-30 10:51:19 +0200

    Lab 4 - 30/05/23 #3 Resolution bug Sensor.cpp & Sensor.h

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -1,17 +1,16 @@
 #include "Sensor.h"
 #include "Date.h"
+#include "Data.h"
+#include "GPS.h"
 #include "Measurement.h"
-#include "Device.h"
-#include <map>
+#include <utility>
 #include <vector>
 #include <cmath>
-#include <iostream>
-#include <chrono>
+#include <ctime>
 
 using namespace std;
 
 Sensor::Sensor(){
-
 }
 
 Sensor::Sensor(string id) {
@@ -26,100 +25,104 @@ bool operator < (const Sensor & unSensor, const Sensor & autreSensor){
   return unSensor.get_id() < autreSensor.get_id();
 }
 
-// bool analyzeSensor(const Sensor& sensor, const Date& start_date) {
-//     bool reliable = true;
-//     int timeRange = 30; // days
-//     double deltaOfReliability = 10;
-//     Date today = chrono::system_clock::now();
-//     Date end_date = start_date+timeRange;
-//
-//     vector<Measure> measures_sensor = get_measures_of_sensor(sensor.get_id(), start_date, end_date);
-//
-//     if ((start_date+timeRange) > today)) {
-//         throw invalid_argument("La période de test doit commencer au moins 30 jours avant aujourd'hui");
-//     } else {
-//         for (auto& m : measures_sensor) {
-//             if (m.value == null || m.value <= 0) {
-//                 reliable = false;
-//                 break;
-//             }
-//         }
-//
-//         vector<double> mean_surroundings = calculateMeanSurroundings(sensor->GPS, start_date, end_date);
-//         vector<double> mean_sensor = calculateMean(sensor, start_date, end_date);
-//
-//         for(int c=0; c<4; c++) {
-//             if (abs(mean_surroundings[0] - mean_sensor[0]) > deltaOfReliability) {
-//                 reliable = false;
-//             }
-//         }
-//     }
-//
-//     return reliable;
-// }
+vector<double> Sensor::calculateMean(const Sensor& sensor, const Date start_date, const Date end_date) {
+    vector<Measurement> measures = Data::get_measures_of_sensor(sensor.get_id(), start_date, end_date);
+    vector<double> mean;
 
-// vector<double> calculateMeanSurroundings(GPS coord, Date start_date, Date end_date) {
-//     vector<pair<Sensor,double>> five_nearest_with_double = get_five_nearest_sensors(coo);
-//     vector<Sensor> = five_nearest
-//
-//     for (auto& p : five_nearest_with_double) {
-//         five_nearest.push_back(p.first);
-//     }
-//
-//     vector<double> mean;
-//
-//     for(auto& s : five_nearest) {
-//         vector<double> result = calculateMean(s, start_date, end_date);
-//         for (int i = 0; i < result.size(); i++) {
-//             mean[i] += result[i];
-//         }
-//     }
-//
-//     for (int i = 0; i < mean.size(); i++) {
-//         mean[i] /= 5;
-//     }
-//
-//     return mean;
-// }
-//
-// vector<double> calculateMean(Sensor sensor, const Date start_date, const Date end_date) {
-//     vector<Measure> measures = get_measures_of_sensor(sensor.get_id(), start_date, end_date);
-//     vector<double> mean;
-//
-//     int count_O3 = 0
-//     int count_NO2 = 0
-//     int count_SO2 = 0
-//     int count_PM10 = 0
-//
+    int count_O3 = 0;
+    int count_NO2 = 0;
+    int count_SO2 = 0;
+    int count_PM10 = 0;
 
-//     for(auto& m : measures){
-//         switch (m->attribute.get_id()) {
-//             case "O3":
-//                 mean[0] += m.value;
-//                 count_O3++;
-//                 break;
-//             case "NO2":
-//                 mean[1] += m.value;
-//                 count_NO2++;
-//                 break;
-//             case "SO2":
-//                 mean[2] += m.value;
-//                 count_SO2++;
-//                 break;
-//             case "PM10":
-//                 mean[3] += m.value;
-//                 count_PM10++;
-//                 break;
-//             default:
-//                 break;
-//         }
-//     }
-//
+    for(auto& m : measures){
+        if(m.get_attribute().get_id() == "O3") {
+            mean[0] += m.get_value();
+            count_O3++;
+            break;
+        }else if(m.get_attribute().get_id() == "NO2") {
+            mean[1] += m.get_value();
+            count_NO2++;
+            break;
+        }else if(m.get_attribute().get_id() == "SO2") {
+            mean[2] += m.get_value();
+            count_SO2++;
+            break;
+        }else if(m.get_attribute().get_id() == "PM10") {
+            mean[3] += m.get_value();
+            count_PM10++;
+            break;
+        }
+    }
 
-//     mean[0] /= count_O3;
-//     mean[1] /= count_NO2;
-//     mean[2] /= count_SO2;
-//     mean[3] /= count_PM10;
-//
-//     return mean;
-// }
+    mean[0] /= count_O3;
+    mean[1] /= count_NO2;
+    mean[2] /= count_SO2;
+    mean[3] /= count_PM10;
+
+    return mean;
+}
+
+vector<double> Sensor::calculateMeanSurroundings(GPS coord, Date start_date, Date end_date) {
+    vector<pair<Sensor,double>> five_nearest_with_double = Data::get_five_nearest_sensors(coord);
+    vector<Sensor> five_nearest;
+
+    for (auto& p : five_nearest_with_double) {
+        five_nearest.push_back(p.first);
+    }
+
+    vector<double> mean;
+
+    for(auto& s : five_nearest) {
+        vector<double> result = calculateMean(s, start_date, end_date);
+        for (int i = 0; i < result.size(); i++) {
+            mean[i] += result[i];
+        }
+    }
+
+    for (int i = 0; i < mean.size(); i++) {
+        mean[i] /= 5;
+    }
+
+    return mean;
+}
+
+bool Sensor::analyzeSensor(const Sensor& sensor, const Date& start_date) {
+    bool reliable = true;
+    int timeRange = 30;  //days
+    double deltaOfReliability = 10;
+
+    time_t currentTime = time(nullptr);
+    struct tm *localTime = localtime(&currentTime);
+    char dateTimeString[20];
+    strftime(dateTimeString, sizeof(dateTimeString), "%d-%m-%Y %H:%M:%S", localTime);
+
+    Date today;
+    today.String_to_time(dateTimeString);
+
+    Date end_date = today;
+    end_date.add_days(timeRange);
+
+    vector<Measurement> measures_sensor = Data::get_measures_of_sensor(sensor.get_id(), start_date, end_date);
+
+    if ((end_date) > today) {
+        throw invalid_argument("La période de test doit commencer au moins 30 jours avant aujourd'hui");
+    } else {
+        for (auto& m : measures_sensor) {
+            if (m.get_value() <= 0) {
+                reliable = false;
+                break;
+            }
+        }
+
+        vector<double> mean_surroundings = calculateMeanSurroundings(sensor.get_coord(), start_date, end_date);
+        vector<double> mean_sensor = calculateMean(sensor, start_date, end_date);
+
+        for(int c=0; c<4; c++) {
+            if (abs(mean_surroundings[0] - mean_sensor[0]) > deltaOfReliability) {
+                reliable = false;
+            }
+        }
+    }
+
+    return reliable;
+}
--- src/Sensor.h
+++ src/Sensor.h
@@ -13,38 +13,37 @@
 
 class Sensor : public Device{
 public:
-  Sensor();
+    Sensor();
 
-  Sensor(std::string id);
+    Sensor(std::string id);
 
-  Sensor(std::string id, bool is_malfunctionning); //Au cas où un user pas trustworthy créee un sensor
+    Sensor(std::string id, bool is_malfunctionning); //Au cas où un user pas trustworthy créee un sensor
 
-  std::map<Sensor, double> scoring_similar_sensors();
+    std::map<Sensor, double> scoring_similar_sensors();
 
-  
 
-  inline bool get_is_malfunctionning() const {
+
+    inline bool get_is_malfunctionning() const {
         return is_malfunctionning;
     }
 
-  inline std::vector<Measurement> get_measurements() const {
+    inline std::vector<Measurement> get_measurements() const {
         return measurements;
     }
 
-  void set_is_malfunctionning(bool malfunctionning);
+    void set_is_malfunctionning(bool malfunctionning);
 
-  void add_measurement(Measurement measurement);
+    void add_measurement(Measurement measurement);
 
-  friend bool operator < (const Sensor & unSensor, const Sensor & autreSensor);
+    friend bool operator < (const Sensor & unSensor, const Sensor & autreSensor);
 
-  bool analyzeSensor(const Sensor& sensor, const Date& start_date);
+    bool analyzeSensor(const Sensor& sensor, const Date& start_date);
 
-  
 protected:
-  bool is_malfunctionning;
-  std::vector<Measurement> measurements;
-  std::vector<double> calculateMean(Sensor sensor, const Date start_date, const Date end_date);
-  std::vector<double> calculateMeanSurroundings(GPS coord, Date start_date, Date end_date);
+    bool is_malfunctionning;
+    std::vector<Measurement> measurements;
+    std::vector<double> calculateMean(const Sensor& sensor, const Date start_date, const Date end_date);
+    std::vector<double> calculateMeanSurroundings(GPS coord, Date start_date, Date end_date);
 };
 
 bool operator < (const Sensor & unSensor, const Sensor & autreSensor);
